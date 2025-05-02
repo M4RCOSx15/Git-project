158b144450129cc6bfa5ce0e9544895d1f0a5ac6
Marcos Gabriel Alves dos Santos <marcos.gabriel.doth15@gmail.com>
2025-05-02 03:26:28 -0300

    sucesso

---
+++ TesteComNum.java
@@ -0,0 +1,12 @@
+package atividade;
+
+public class TesteComNum {
+    public static void main(String[]args){
+        int i;
+        for(i=1;i<=20;i++){
+            System.out.print(i);
+            System.out.println(i+"  ");
+        }
+    }
+    
+}
